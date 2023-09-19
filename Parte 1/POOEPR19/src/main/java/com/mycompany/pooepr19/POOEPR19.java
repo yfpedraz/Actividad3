f33d4a9aa40f7b9c9026ac7da25298c7c97f4cbb
yfpedraz <yfpedraz@unal.edu.co>
2023-09-18 22:14:48 -0500

    Ejercicio 19

---
+++ Parte 1/POOEPR19/src/main/java/com/mycompany/pooepr19/POOEPR19.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooepr19;
+
+/**
+ *
+ * @author YONNATTANTH
+ */
+public class POOEPR19 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
